a398c75f4e30735511e7deaab261b11272e1dfc3
Rivu Khoda <rivu.khoda@gmail.com>
2016-02-21 06:17:12 -0500

    WIP: Added new credentials

--- backend/modules/curl_example1.sh
+++ backend/modules/curl_example1.sh
@@ -3,7 +3,7 @@
 
 for file in ../temp/*
 do
-	curl "https://dictation.nuancemobility.net:443/NMDPAsrCmdServlet/dictation?appId=NMDPTRIAL_rivu_khoda_gmail_com20160220161406&appKey=6a4a57c8ddf643690d85cc180e1d9468ec8abd5fc169a2e499710b6169cb1ed415759cf26bdf5b78652790734424db30af998f700231549ca25ffd0c2a1be03f&id=C4461956B60B" -H "Content-Type: audio/x-wav;codec=pcm;bit=16;rate=16000" -H "X-Dictation-NBestListSize: 1" -H "Accept-Language: ENUS" -H "Transfer-Encoding: chunked" -H "Accept: application/xml" -H "Accept-Topic: Dictation" -k --data-binary @../temp/$filename -v >> ../results/$count &
+	curl "https://dictation.nuancemobility.net:443/NMDPAsrCmdServlet/dictation?appId=NMDPTRIAL_xun_su_mail_mcgill_ca20160220144129&appKey=86af83f5902afa384a1614acf23a7bcca812cb9bc1420f716c46fbc5a11e2d27cdb11d5ddb1795252daad11fe44deb8c5fb72820f93791657e8c2a1fab584678" -H "Content-Type: audio/x-wav;codec=pcm;bit=16;rate=16000" -H "X-Dictation-NBestListSize: 1" -H "Accept-Language: ENUS" -H "Transfer-Encoding: chunked" -H "Accept: application/xml" -H "Accept-Topic: Dictation" -k --data-binary @../temp/$filename -v >> ../results/$count &
 	let "count=count+5"
 	let "filename=filename+1"
 done
